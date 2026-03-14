00f9f56dbff1f9f667f7433d00e9fb829e54219a
Donsig <a@donsig.dk>
2026-03-14 22:23:20 +0100

    feat(hooks): add useNow shared interval hook

--- src/hooks/__tests__/useNow.test.ts
+++ src/hooks/__tests__/useNow.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useNow } from '../useNow.ts';
 
 describe('useNow', () => {
